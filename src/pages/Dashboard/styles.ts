cb999776674baf1bb9a0e5929115774f9f19889c
Dakson Cruz <daksonc@gmail.com>
2022-10-27 12:40:36 -0300

    feat: intala styled-components

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #312e38;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  margin: 64px 0 24px;
+`;
