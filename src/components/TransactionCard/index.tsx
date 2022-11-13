3794127304c3159916b0af02e0871e4ac4663edc
Dakson Cruz <daksonc@gmail.com>
2022-11-13 11:43:38 -0300

    refactor Amout do TransactionCard

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -33,7 +33,7 @@ export const TransactionCard = ({ data }: Data) => {
         {data.name}
       </Title>
       <Amount type={data.type}>
-        {data.type === 'negative' && '- '}
+        {data.type === 'negative' ? '- ' : '+ '}
         {data.amount}
       </Amount>
       <Footer>
